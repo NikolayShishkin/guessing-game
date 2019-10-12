3e40b4061fc0f667c4713499fb251626205172c2
NikolayShishkin <r58korin@gmail.com>
2019-10-12 20:02:31 +0300

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minRange = 0;
+        this.maxRange = 0;
+        this.currentTry = 0;
+    }
 
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
     }
 
     guess() {
-
+        this.currentTry = Math.round((this.minRange + this.maxRange)/2);
+        return this.currentTry;
     }
 
     lower() {
-
+        this.maxRange = this.currentTry;
     }
 
     greater() {
-
+        this.minRange = this.currentTry;
     }
 }
 
